a4fd2a43a3bad9b415146fb23d24b7a78fb0faed
Yalın Pala <yalinpala@protonmail.com>
2021-02-28 13:50:01 +0300

    niminy timer

--- src/extension.js
+++ src/extension.js
@@ -1,19 +1,18 @@
 const vscode = require('vscode');
 
-let myStatusBarItem = vscode.StatusBarItem;
+let timerStatusBarItem = vscode.StatusBarItem;
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	console.log('Congratulations, your extension "niminy" is now active!');
 
-	let disposable = context.subscriptions.push(vscode.commands.registerCommand('niminy.helloWorld', () => {
+	let disposable = context.subscriptions.push(vscode.commands.registerCommand('niminy.timer', () => {
 		timePicker().then(m => {
-			console.log(m);
-			myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-			myStatusBarItem.command = 'niminy.helloWorld';
-			context.subscriptions.push(myStatusBarItem);
+
+			timerStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+			timerStatusBarItem.command = 'niminy.timer';
+			context.subscriptions.push(timerStatusBarItem);
 
 			context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(countDown(m)));
 			context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(countDown(m)));
@@ -21,36 +20,38 @@ function activate(context) {
 	}));
 
 	context.subscriptions.push(disposable);
+
 }
 
 function deactivate() {}
 
 function countDown(m) {
-	let cd = m;
+	let seconds = m * 60;
 	
 	let x = setInterval(function() {
-		// Find the distance between now and the count down date
-		cd--;
-		// If the count down is finished, write some text
-		if (cd < 0) {
+		seconds--;
+
+		if (seconds < 0) {
 		  clearInterval(x);
-		  vscode.window.showInformationMessage(`Time is over!`);
-		  myStatusBarItem.hide();
+		  vscode.window.showInformationMessage('Time is over!');
+		  timerStatusBarItem.hide();
 		} else {
-			myStatusBarItem.text = `$(megaphone) ${cd} sec`;
-			myStatusBarItem.show();
+			let minute = Math.floor(seconds / 60);
+			let second = seconds % 60;
+			timerStatusBarItem.text = `$(watch) ${minute}:${second}`;
+			timerStatusBarItem.show();
 		}
 	  }, 1000);
 }
 
 async function timePicker() {
-	const result = await vscode.window.showQuickPick(['20', '30', '40', '60', '120'], {
+	const minute = await vscode.window.showQuickPick(['20', '30', '40', '60', '120', '240'], {
 		placeHolder: 'How many minutes would you like me to warn you later?',
 	});
 
-	vscode.window.showInformationMessage(`OK, I'll warn you in ${result} minutes.`);
+	vscode.window.showInformationMessage(`OK, I'll warn you in ${minute} minutes.`);
 
-	return result;
+	return minute;
 }
 
 
